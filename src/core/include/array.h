ec9036cfec33f762b9ee42ad1c5a9c865b6528b9
Simone Micucci <s.micucci00@gmail.com>
2025-06-02 00:29:22 +0200

    add(array): base array implementation, redesign of interfaces

--- src/core/include/array.h
+++ src/core/include/array.h
@@ -3,10 +3,12 @@
 
 #include <cops/core/container.h>
 #include <cops/core/interface.h>
-#include <cstddef>
+#include <cops/core/iterator.h>
 #include <stddef.h>
 
 typedef struct cops_array cops_array_t;
+
+extern const cops_interface_t *const Array;
 
 struct cops_array {
         cops_container_t super;
@@ -14,7 +16,14 @@ struct cops_array {
         size_t len;
 };
 
-void array_init(cops_array_t *self, cops_interface_t type, size_t len);
+void array_init(cops_array_t *self, cops_interface_t *type, size_t len);
 void array_clean(cops_array_t *self);
+cops_iterator_t *array_iterator(cops_array_t *self);
+void array_merge(cops_array_t *self, cops_container_t *input);
+
+void array_insert(cops_array_t *self, size_t pos, void *val);
+void *array_remove(cops_array_t *self, size_t pos);
+void array_set(cops_array_t *self, size_t pos, void *val);
+void *array_peek(cops_array_t *self, size_t pos);
 
 #endif /* end of include guard: COPS_CORE_ARRAY_H */
--- src/core/include/container.h
+++ src/core/include/container.h
@@ -9,17 +9,12 @@
 typedef struct cops_container cops_container_t;
 
 typedef cops_iterator_t *(*cops_iterator_FnPtr)(cops_container_t *);
-typedef void (*cops_insert_FnPtr)(cops_container_t *, const void *);
-typedef void *(*cops_remove_FnPtr)(cops_container_t *);
-typedef cops_container_t *(*cops_toarray_FnPtr)(cops_container_t *,
-                                                cops_container_t *);
+typedef void *(*cops_toarray_FnPtr)(cops_container_t *);
 typedef void (*cops_merge_FnPtr)(cops_container_t *, cops_container_t *);
 
 struct cops_container {
         cops_interface_t super;
         cops_iterator_FnPtr iter;
-        cops_insert_FnPtr insert;
-        cops_remove_FnPtr remove;
         cops_toarray_FnPtr toarr;
         cops_merge_FnPtr merge;
         cops_interface_t *value;
